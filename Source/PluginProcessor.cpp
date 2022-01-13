afe7408f75ac651ae40ca32d1bf66637244d220e
Eamon McMullen <drover700@yahoo.com>
2022-01-13 08:22:22 -0800

    Allowed plugin to save state when exiting DAW
    and load the state back up in the DAW so you can stop mixing for a day and still use your settings as intended the next day

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -22,7 +22,7 @@ SlapsAudioProcessor::SlapsAudioProcessor()
                        ), apvts (*this, nullptr, "Parameters", createParameters())
 #endif
 {
-   
+
 }
 
 SlapsAudioProcessor::~SlapsAudioProcessor()
@@ -393,7 +393,9 @@ void SlapsAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
-
+    auto state = apvts.copyState();
+    std::unique_ptr<juce::XmlElement> xml(state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 
@@ -402,7 +404,15 @@ void SlapsAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> theParams(getXmlFromBinary(data, sizeInBytes));
 
+    if (theParams != nullptr)
+    {
+        if (theParams->hasTagName(apvts.state.getType()))
+        {
+            apvts.replaceState (juce::ValueTree::fromXml(*theParams));
+        }
+    }
 }
 
 
